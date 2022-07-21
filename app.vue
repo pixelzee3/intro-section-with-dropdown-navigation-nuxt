4bb86df4f62851c8e4add152c8f708479641dbe5
PixelZee3 <bpixelzee@gmail.com>
2022-07-21 20:21:51 +0700

    Remove test code.

--- app.vue
+++ app.vue
@@ -2,8 +2,5 @@
   <Header />
   <main>
     <Hero class="pt-6" />
-    <Container class="pt-6">
-      <div class="w-16 h-16 m-auto bg-red-400 hover:bg-blue-400"></div>
-    </Container>
   </main>
 </template>
